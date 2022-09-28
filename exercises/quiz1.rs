0c0814dee40de15ae2c518b7236b38fb1dce0ce4
Hugues400 <61408049+Hugues400@users.noreply.github.com>
2022-09-28 14:33:30 +0100

    commit

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,12 +8,18 @@
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given
-// the quantity bought. No hints this time!
-
-// I AM NOT DONE
+// the quantity bought. No hints this time
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (price:i32) ->i32 {
+    if price<=40 {
+        return price*2;
+    }
+    else {
+        return price;
+    }
+
+}
 
 // Don't modify this function!
 #[test]
